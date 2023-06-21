a17902e9279561a8890a759bb06ecf2add3f83af
iamtaufik <muhtopik07@gmail.com>
2023-06-21 19:38:14 +0700

    add scale and face enchance

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.post('/api/upload', async (req, res) => {
     const output = await replicate.run('nightmareai/real-esrgan:42fed1c4974146d4d2414e2be2c5277c7fcf05fcc3a73abf41610695738c1d7b', {
       input: {
         image: uploadResponse.url,
-        scale: scale,
+        scale: parseInt(scale),
         face_enchance: face_enchance,
       },
     });
